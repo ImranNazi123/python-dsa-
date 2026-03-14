57fc977af2fa2f7de6404a939fe47973f5abd125
Imran Nazi <54521300+ImranNazi123@users.noreply.github.com>
2026-03-14 23:39:34 +0530

    codeforces three bro , finding , who came late...
    worked well in java !!!!!

---
+++ python dsa/ThreeBrothers.py
@@ -0,0 +1,20 @@
+n=6
+a=int(input())
+b=int(input())
+sum=0
+
+
+sum+=a+b 
+    # print(n-sum,"bro was late")
+print(n-sum)
+
+
+def bro(n,a,b,sum):
+    sum+=a+b 
+    # print(n-sum,"bro was late")
+    print(n-sum)
+
+
+# bro(n,a,b,sum)
+
+   
